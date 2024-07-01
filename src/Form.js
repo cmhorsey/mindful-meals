d68dc6d4678f69030007481dae330023e01808ca
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-01 13:38:19 -0400

    Add date picker to form

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 const initialValue = {
+  date: new Date(),
   breakfast: "",
   lunch: "",
   dinner: "",
@@ -9,6 +12,7 @@ const initialValue = {
 function Form({ handleChange }) {
   const [formData, setFormData] = useState(initialValue)
 
+  const date = formData.date
   const breakfast = formData.breakfast
   const lunch = formData.lunch
   const dinner = formData.dinner
@@ -21,22 +25,39 @@ function Form({ handleChange }) {
     })
   }
 
+  const handleDateChange = (date) => {
+    setFormData({ ...formData, date })
+  }
+
   function handleSubmit(event) {
     event.preventDefault()
+    console.log(query)
+    console.log(date)
 
-    fetch("https://api.calorieninjas.com/v1/nutrition?query=" + query, {
-      method: "GET",
-      headers: {
-        "X-Api-Key": "jrQyEj+ffbtZSdfVrr8HJQ==V3J3FdNbQrPL65DI",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+    // fetch("https://api.calorieninjas.com/v1/nutrition?query=" + query, {
+    //   method: "GET",
+    //   headers: {
+    //     "X-Api-Key": "jrQyEj+ffbtZSdfVrr8HJQ==V3J3FdNbQrPL65DI",
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
   }
 
   return (
     <form onSubmit={handleSubmit} className="container mt-4">
+      <div className="mb-3">
+        <label htmlFor="date" className="form-label">
+          Date
+        </label>
+        <DatePicker
+          selected={formData.date}
+          onChange={handleDateChange}
+          className="form-control"
+          dateFormat="MM/dd/yyyy"
+        />
+      </div>
       <div className="mb-3">
         <label htmlFor="breakfast" className="form-label">
           Breakfast
