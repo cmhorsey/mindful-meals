d83a23b3b97c8ee8b7dbab014a37ac5f8dd4f1c0
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-12 09:45:38 -0400

    Add DevTools and delete unnecessary form handlers

--- src/components/meal-log/Form.tsx
+++ src/components/meal-log/Form.tsx
@@ -2,80 +2,103 @@ import React from "react"
 import CustomDatePicker from "../CustomDatePicker"
 import "../../styles/index.css"
 import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
 
-interface FormProps {
-  formData: FormData
-  onHandleDateChange: () => void
-  onSubmitForm: () => void
-  onFormChange: () => void
-}
+// interface FormProps {
+//   formData: FormData
+//   onHandleDateChange: () => void
+//   onSubmitForm: () => void
+//   onFormChange: () => void
+// }
 
-interface FormData {
+type FormValues = {
   date: Date
   breakfast: string
   lunch: string
   dinner: string
 }
 
-function Form({
-  formData,
-  onFormChange,
-  onHandleDateChange,
-  onSubmitForm,
-}: FormProps) {
+function Form({}) {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      date: new Date(),
+    },
+    mode: "onBlur",
+  })
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState,
+    watch,
+    getValues,
+    setValue,
+    reset,
+    trigger,
+  } = form
+  const {} = formState
+
   return (
-    <form onSubmit={onSubmitForm} className="container form-container">
-      <h2 className="form-header">Add Meals</h2>
-      <CustomDatePicker
-        selectedDate={formData.date}
-        onHandleDateChange={onHandleDateChange}
-      />
-      <div className="mb-3">
-        <label htmlFor="breakfast" className="form-label">
-          Breakfast
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="breakfast"
-          name="breakfast"
-          value={formData.breakfast}
-          onChange={onFormChange}
-          placeholder="What did you have for breakfast?"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="lunch" className="form-label">
-          Lunch
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="lunch"
-          name="lunch"
-          value={formData.lunch}
-          onChange={onFormChange}
-          placeholder="What did you have for lunch?"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="dinner" className="form-label">
-          Dinner
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="dinner"
-          name="dinner"
-          value={formData.dinner}
-          onChange={onFormChange}
-          placeholder="What did you have for dinner?"
-        />
-      </div>
-      <button type="submit" className="btn btn-secondary btn-submit">
-        Submit
-      </button>
-    </form>
+    <div>
+      <form className="container form-container">
+        <h2 className="form-header">Add Meals</h2>
+        <div className="form-control">
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            {...register("date", {
+              valueAsDate: true,
+            })}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="breakfast" className="form-label">
+            Breakfast
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="breakfast"
+            name="breakfast"
+            placeholder="What did you have for breakfast?"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="lunch" className="form-label">
+            Lunch
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lunch"
+            name="lunch"
+            placeholder="What did you have for lunch?"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="dinner" className="form-label">
+            Dinner
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="dinner"
+            name="dinner"
+            placeholder="What did you have for dinner?"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-secondary btn-submit">
+          Submit
+        </button>
+      </form>
+      <DevTool control={control} />
+    </div>
   )
 }
 
